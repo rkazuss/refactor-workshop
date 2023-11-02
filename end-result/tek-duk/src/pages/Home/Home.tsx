c55d0c75b1c297051c1fd7aa9aee0ddd70ddb54e
rkazuss <raimonds.kazuss@visma.com>
2023-11-02 13:52:19 +0200

    added loading and error state

--- end-result/tek-duk/src/pages/Home/Home.tsx
+++ end-result/tek-duk/src/pages/Home/Home.tsx
@@ -4,14 +4,23 @@ import { Product } from "../../shared/interfaces";
 import ProductItem from "../../components/Product/ProductItem";
 
 const Home = () => {
-  const query = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["products"],
     queryFn: apiClient.getProducts,
   });
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Oops! Something went wrong...</div>;
+  }
+
+
   return (
     <div>
-      {query.data?.data.products.map((product: Product) => (
+      {data?.data.products.map((product: Product) => (
         <ProductItem
           key={product.id}
           name={product.title}
