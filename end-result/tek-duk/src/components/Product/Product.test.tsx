574a4630e7295cecfd7b4c6698543606a2fd509d
rkazuss <raimonds.kazuss@visma.com>
2023-11-02 13:31:27 +0200

    improvements

--- end-result/tek-duk/src/components/Product/Product.test.tsx
+++ end-result/tek-duk/src/components/Product/Product.test.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Product from "./Product";
 
-test("should render a product item", () => {
-  render(<Product />);
-  expect(screen.getByTestId("product_title")).toBeInTheDocument();
+test("should render a product item with data from props", () => {
+  render(<Product name="some product" price="88.88" />);
   expect(screen.getByTestId("product_thumbnail")).toBeInTheDocument();
+  expect(screen.getByTestId("product_name")).toBeInTheDocument();
   expect(screen.getByTestId("product_price")).toBeInTheDocument();
+  expect(screen.getByText("some product")).toBeInTheDocument();
+  expect(screen.getByText("88.88")).toBeInTheDocument();
 });
--- end-result/tek-duk/src/components/Product/Product.tsx
+++ end-result/tek-duk/src/components/Product/Product.tsx
@@ -1,9 +1,17 @@
-const Product = () => {
+interface Props {
+  imgSrc?: string;
+  imgAlt?: string;
+  name: string;
+  description?: string;
+  price: string;
+}
+
+const Product = ({ imgSrc, imgAlt, name, description, price }: Props) => {
   return (
-    <div>
-      <img src="" alt="" data-testid="product_thumbnail" />
-      <div data-testid="product_title">Product name</div>
-      <div data-testid="product_price">88.88</div>
+    <div aria-label={description}>
+      <img src={imgSrc} alt={imgAlt} data-testid="product_thumbnail" />
+      <div data-testid="product_name">{name}</div>
+      <div data-testid="product_price">{price}</div>
     </div>
   );
 };
